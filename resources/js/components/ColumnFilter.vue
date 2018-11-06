5c40b649d5960bb7f7f1bc9f3fbf4bad04ff0440
Philippe Pérusse <pperusse@globalia.ca>
2018-11-06 09:54:09 -0500

    accept string as value

--- resources/js/components/ColumnFilter.vue
+++ resources/js/components/ColumnFilter.vue
@@ -52,7 +52,7 @@
     export default {
         props: {
             value : {
-                type: Object,
+                type: [String, Object],
                 default : () =>  ({
                     column : '',
                     operator : '',
